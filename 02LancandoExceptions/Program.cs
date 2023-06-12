5ab54e1f30d182da24769f986ded9be0695328af
Jessé Caldas <jessencaldas@hotmail.com>
2023-06-12 16:38:31 -0300

    add 02

---
+++ 02LancandoExceptions/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("Lançando Exceções\n");
+
+
+try
+{
+    A.ProcessarA();
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine("\nTratando o erro em Main\n");
+    Console.WriteLine(ex.Message);
+    Console.WriteLine(ex.StackTrace);
+}
+
+Console.ReadKey();
+class A
+{
+    public static void ProcessarA()
+    {
+        //Se o tratamento está aqui, ele trata e não passa
+        try
+        {
+            B.ProcessarB();
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Tratando Erro em A");
+        }
+    }
+}
+
+class B
+{
+    public static void ProcessarB()
+    {
+        C.ProcessarC();
+    }
+}
+
+class C
+{
+    public static void ProcessarC()
+    {
+        throw new NotImplementedException("Método não implementado");
+    }
+}
